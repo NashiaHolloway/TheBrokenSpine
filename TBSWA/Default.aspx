ae24ca5c678609483302f4105baa16dc5bb3bb98
Nashia Holloway <nashiaholloway1@gmail.com>
2017-09-26 18:13:19 -0700

    Charts and Graphs powerpoint

--- TBSWA/Default.aspx
+++ TBSWA/Default.aspx
@@ -1,6 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Bookstore.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TBSWA.Default" %>
+<%@ Register Assembly="ZedGraph.Web" Namespace="ZedGraph.Web" TagPrefix="cc1" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     Default Page!!
+    <cc1:ZedGraphWeb ID="ZedGraphWeb1" runat="server" OnRenderGraph="ZedGraphWeb1_RenderGraph"></cc1:ZedGraphWeb>
 </asp:Content>
--- TBSWA/Default.aspx.cs
+++ TBSWA/Default.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ZedGraph;
+using ZedGraph.Web;
 
 namespace TBSWA
 {
@@ -12,6 +15,30 @@ namespace TBSWA
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void ZedGraphWeb1_RenderGraph(ZedGraphWeb webObject, Graphics g, MasterPane pane)
+        {
+            //GraphPane class came from the ZedGraph DLL
+            GraphPane myPane = pane[0]; //create an empty Graph Pane object
+            myPane.Title.Text = "Title"; //Title text property of pane object
+            myPane.XAxis.Title.Text = "X axis label"; //Title X axis label text property
+            myPane.YAxis.Title.Text = "Y axis label"; //Title Y axis label text property
+            //PointPairList class came from the ZedGraph DLL
+            PointPairList list1 = new PointPairList(); //New Point Pair List object
+            //Create some plotting line to display
+            for (int i = 0; i < 10; i++)
+            {
+                double x = Convert.ToDouble(i);
+                double y = x * x;
+                list1.Add(x, y);
+            }
+            //Use a LineItem type chart came from the ZedGraph DLL
+            string textForLegend = "x-squared";
+            myPane.CurveList.Clear();
+            LineItem myCurve = myPane.AddCurve(textForLegend,
+              list1, Color.Red, SymbolType.Diamond);
+            myCurve.Symbol.IsVisible = true;
         }
     }
 }
