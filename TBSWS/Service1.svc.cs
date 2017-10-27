ad05e1037c13777a68a3111488bc4147f72bef45
Nashia Holloway <nashiaholloway1@gmail.com>
2017-10-26 18:16:26 -0700

    Added a web service

---
+++ TBSWS/Service1.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using TBSWS.App_Code;
+
+namespace TBSWS
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public string AddTwoNumbers(int num1, int num2)
+        {
+            int value = num1 + num2;
+            return string.Format("Sum: {0}", value);
+        }
+
+        public string SubTwoNumbers(int num1, int num2)
+        {
+            int value = num1 - num2;
+            return string.Format("Sum: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
