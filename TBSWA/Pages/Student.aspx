f115936a6c863f1f19c6058a7763c0ccf734f331
Nashia Holloway <nashiaholloway1@gmail.com>
2017-11-16 18:23:57 -0800

    Entity Framework things

---
+++ TBSWA/Pages/Student.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Bookstore.Master" AutoEventWireup="true" CodeBehind="Student.aspx.cs" Inherits="TBSWA.Pages.Student" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    
+<%--<asp:GridView ID="GridView1" runat="server" Style="clear:both"></asp:GridView>--%>
+
+</asp:Content>
---
+++ TBSWA/Pages/Student.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TBSWA.Pages
+{
+    public partial class Student : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (TheBrokenSpineEntities entities = new TheBrokenSpineEntities())
+            {
+                //Insert a Record
+                //===================================
+                //Create the student stud obect
+                tblStudent stud = new tblStudent();
+                //fill the object with data
+                stud.fldStudentName = "Joe Oakes";
+                stud.fldStudentSSN = "666666666";
+                stud.fldStudentEmail = "joe.oakes@psu.edu";
+                stud.fldStudentPhone = "555-555-5555";
+                //Call the method to add the object to the table
+                entities.tblStudents.Add(stud);
+                //Save the changes to the DB
+                entities.SaveChanges();
+                //stud.fldStudentID will contain the record number
+
+                //Update a record
+                //==================================
+                stud.fldStudentName = "Joseph Oakes";
+                entities.SaveChanges();
+
+                //Delete a record
+                //==================================
+                //LINQ
+
+                //var stud = from s in entities.tblStudents
+                //           where s.fldStudentID == 1
+                //           select s;
+
+                //entities.tblStudents.Remove(stud);
+                //entities.SaveChanges();
+
+            }
+        }
+    }
+}
